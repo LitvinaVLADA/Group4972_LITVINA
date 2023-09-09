84b605f8fedc8348892bd89591670dbcc749c318
LitvinaVlada <vladamay@outlook.com>
2023-09-09 20:55:38 +0300

    seminar1_homework2

--- seminar1_task8/Program.cs
+++ seminar1_task8/Program.cs
@@ -1,2 +1,13 @@
 ﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
+Console.Write ("Введите число: ");
+int numb = int.Parse(Console.ReadLine()??"0"); //Получаем число и записываем его в переменную
+int count = 2;
+
+while (count<numb)
+{
+    Console.Write (count%2==0 ? count:", ");
+    count ++; //счётчик
+}
+Console.Write (numb%2==0 ? numb:"");
+
