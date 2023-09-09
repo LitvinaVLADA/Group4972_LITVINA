7c7c11c9511feaf9d810736a7f0a520b4a12455f
LitvinaVlada <vladamay@outlook.com>
2023-09-09 21:39:06 +0300

    seminar2_homework1

--- seminar2_task10/Program.cs
+++ seminar2_task10/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// №10 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write ("Введите трёхзначное число: ");
+int numb = int.Parse(Console.ReadLine()??"0"); //Получаем число и записываем его в переменную
+
+if (numb>99&&numb<1000) //проверяем на трёхзначность
+{
+    int result = (numb%100-numb%10)/10; //высчитываем какая цифра является второй
+    Console.WriteLine (result);
+}
+else 
+{
+    Console.WriteLine("Число не трёхзначное. Повторите попытку."); //если число не трёхзначное выводим сообщение об ошибке
+}
--- seminar2_task15/Program.cs
+++ seminar2_task15/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// №15 Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void Variant1() //создаём первый метод
+{
+    Console.WriteLine("Способ 1");
+
+    Console.Write("Введите номер дня недели: ");
+    int NumbDay = int.Parse(Console.ReadLine()??"0");
+    string [] WeekDays = new string [7]; //создаём коллекцию где номер дня недели соответствует результату
+    WeekDays[0] = "Понедельник не является выходным";
+    WeekDays[1] = "Вторник не является выходным";
+    WeekDays[2] = "Среда не является выходным";
+    WeekDays[3] = "Четверг не является выходным";
+    WeekDays[4] = "Пятница не является выходным";
+    WeekDays[5] = "Суббота является выходным";
+    WeekDays[6] = "Воскресенье является выходным";
+    Console.WriteLine (NumbDay<8&&NumbDay>0 ? WeekDays[NumbDay-1]: "Нет такого дня недели"); //выводим результат исходя из введёного номера исключая ошибки
+}
+
+void Variant2() //создаём второй метод
+{
+    Console.WriteLine("Способ 2");
+
+    Console.Write("Введите номер дня недели: ");
+    int NumbDay = int.Parse(Console.ReadLine()??"0");
+    if (NumbDay<8&&NumbDay>0) //проверяем на ошибки введённое число
+    {
+        Console.WriteLine (NumbDay<6 ? "Этот день не выходной":"Этот день выходной"); //выводим результат
+    }
+    else Console.WriteLine ("Нет такого дня недели"); //результат в случае ошибки при вводе
+}
+
+Variant1(); //выводим первый метод
+Variant2(); //выводим второй метод
