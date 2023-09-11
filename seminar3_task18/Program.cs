10ae20a0d5b47f71a8eea0eef7f7de469cf192b3
LitvinaVlada <vladamay@outlook.com>
2023-09-11 23:08:32 +0300

    seminar3_part1

---
+++ seminar3_task18/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 18: Напишите программу, которая по 
+// заданному номеру четверти, показывает 
+// диапазон возможных координат точек в этой 
+// четверти (x и y).
+
+Console.WriteLine("Введите номер четверти где находится точка:");
+int quarter = Convert.ToInt32(Console.ReadLine()??"0");
+int xMin, xMax, yMin, yMax;
+
+switch (quarter)
+    {
+        case 1:
+                xMin = 0;
+                xMax = int.MaxValue;
+                yMin = 0;
+                yMax = int.MaxValue;
+                break;
+        case 2:
+                xMin = int.MinValue;
+                xMax = 0;
+                yMin = 0;
+                yMax = int.MaxValue;
+                break;
+        case 3:
+                xMin = int.MinValue;
+                xMax = 0;
+                yMin = int.MinValue;
+                yMax = 0;
+                break;
+        case 4:
+                xMin = 0;
+                xMax = int.MaxValue;
+                yMin = int.MinValue;
+                yMax = 0;
+                break;
+        default:
+                Console.WriteLine("Неверный номер четверти");
+                return;
+    }
+
+Console.WriteLine($"Диапазон координат точек в четверти {quarter}:");
+Console.WriteLine($"x: от {xMin} до {xMax}");
+Console.WriteLine($"y: от {yMin} до {yMax}");
---
+++ seminar3_task21/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 21: Напишите программу, которая 
+// принимает на вход координаты двух точек и 
+// находит расстояние между ними в 2D 
+// пространстве
+
+
+Console.WriteLine("Введите координаты первой точки (x1, y1):");
+        double x1 = double.Parse(Console.ReadLine()??"0");
+        double y1 = double.Parse(Console.ReadLine()??"0");
+
+        Console.WriteLine("Введите координаты второй точки (x2, y2):");
+        double x2 = double.Parse(Console.ReadLine()??"0");
+        double y2 = double.Parse(Console.ReadLine()??"0");
+
+        double distance = Math.Round(Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)),2);
+
+        Console.WriteLine($"Расстояние между точками ({x1}, {y1}) и ({x2}, {y2}) равно {distance}");
