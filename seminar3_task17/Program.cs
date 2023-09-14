0a1a6fa6d2f6229b3a27efc5aefacdc7a8521716
LitvinaVlada <vladamay@outlook.com>
2023-09-14 21:48:40 +0300

    seminar3_part2

--- seminar3_task17/Program.cs
+++ seminar3_task17/Program.cs
@@ -3,30 +3,32 @@
 // номер четверти плоскости, в которой находится эта 
 // точка
 
+int Read_Data (string start) //метод для получения координат
+{
+    Console.WriteLine(start);
+    int num = int.Parse(Console.ReadLine()??"0"); 
+    return num;
+}
 
-Console.WriteLine("Введите координаты точки (X и Y):");
-double x = double.Parse(Console.ReadLine()??"0"); //Получаем первое число и записываем его в переменную Х
-double y = double.Parse(Console.ReadLine()??"0"); //Получаем второе число и записываем его в переменную У
+int GetQuarter(int x, int y) //метод для нахождения четверти
+{
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    else return 4;
+}
 
-int quarter = GetQuarter(x, y); //С помощью функции находим в какой четверти лежит наша точка
-Console.WriteLine("Точка находится в " + quarter + " четверти плоскости."); //Выводим результат
+void Print_Result (string msg)
+{
+    Console.WriteLine(msg);
+}
 
-static int GetQuarter(double x, double y) //Описание функции для нахождения четверти
-    {
-        if (x > 0 && y > 0)
-        {
-            return 1;
-        }
-        else if (x < 0 && y > 0)
-        {
-            return 2;
-        }
-        else if (x < 0 && y < 0)
-        {
-            return 3;
-        }
-        else
-        {
-            return 4;
-        }
-    }
+
+
+int x = Read_Data("Введите координату точки X:");//Получаем первое число и записываем его в переменную Х
+int y = Read_Data("Введите координату точки Y:");//Получаем второе число и записываем его в переменную У
+
+int result = GetQuarter(x,y);
+
+Print_Result ("Точка находится в "+result+" четверти плоскости");
+
---
+++ seminar4_task24/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 24: Напишите программу, которая 
+// принимает на вход число (А) и выдаёт сумму чисел 
+// от 1 до А.
+
+int Read_Data (string numb)
+Console.WriteLine("Введите число: ");
+int numb = int.Parse(Console.ReadLine()??"0");
