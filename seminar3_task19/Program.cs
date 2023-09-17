5327220f9ace5aee7fb438e29012b15126628b14
LitvinaVlada <vladamay@outlook.com>
2023-09-17 19:24:47 +0300

    seminar3_homework1

---
+++ seminar3_task19/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 19: Напишите программу, которая принимает на 
+// вход пятизначное число и проверяет, является ли оно 
+// палиндромом.
+
+
+Console.Write("Введите пятизначное число: "); //получаем число и проверяем пятизначное ли оно
+int number = int.Parse(Console.ReadLine()??"0");
+if (number < 10000 || number > 99999)
+    {
+    Console.WriteLine("Число не пятизначное. Попробуйте ещё раз.");
+    }
+else{
+    if (Palindrome_check(number)) //производим проверку на палиндром с нашим числом и выводим результат
+        {
+        Console.WriteLine("Данное число является палиндромом.");
+        }
+    else
+        {
+            Console.WriteLine("Данное число не является палиндромом.");
+        }
+}
+
+
+bool Palindrome_check(int number) //метод для проверки на палиндром
+{
+    Dictionary<int, int> digits = new Dictionary<int, int>();
+
+    // Разбиваем число на отдельные цифры и сохраняем их в словаре
+    int digitPosition = 0;
+    int digit = number % 10;
+    digits.Add(digitPosition, digit);
+    digitPosition++;
+    number = number / 10;
+
+    // Проверяем, является ли число палиндромом через сверку противостоящих чисел
+    for (int i = 0; i < digits.Count / 2; i++)
+    {
+        if (digits[i] != digits[digitPosition - i - 1])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
---
+++ seminar3_task23/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N
+
+int Read_Data (string start) //метод для получения числа N
+{
+    Console.Write(start);
+    int num = int.Parse(Console.ReadLine()??"0"); 
+    return num;
+}
+
+string Build_Line (int num, int pow) //метод для вывода строки с числами с учетом куба
+{
+    string result = "";
+    for (int i = 1; i <= num; i++)
+    {
+        result = result + Math.Pow(i,pow)+" ";
+    }
+    return result;
+}
+
+
+void Print_Result (string msg) //метод для вывода результата
+{
+    Console.WriteLine(msg);
+}
+
+int n = Read_Data ("Введите число: ");
+Print_Result (Build_Line (n, 1)); //первая строка где перечисляются числа
+Print_Result (Build_Line (n, 3)); //вторая строка где указываются кубы чисел
