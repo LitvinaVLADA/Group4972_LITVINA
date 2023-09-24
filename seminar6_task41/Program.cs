0a38d573e1b4e8636654b8961e9d0dc1f6e46a4f
LitvinaVlada <vladamay@outlook.com>
2023-09-24 19:58:18 +0300

    seminar6_homework1

---
+++ seminar6_task41/Program.cs
@@ -0,0 +1,28 @@
+﻿// №41 Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+int Read_Data(string start) //метод для получения числа
+{
+    Console.Write(start);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+int Count_Positive_Numbers(int count) //метод дл введения пользователем чисел и одновременной проверки на >0
+{  
+    int positiveCount = 0;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"Введите число {i + 1}: ");
+        int number = int.Parse(Console.ReadLine()?? "0");
+        if (number > 0)
+        {
+            positiveCount++;
+        }
+    }
+    
+    return positiveCount;
+}
+
+int count = Read_Data("Введите количество чисел: ");
+int result = Count_Positive_Numbers (count);
+Console.WriteLine ("Количество чисел больше 0: "+result);
---
+++ seminar6_task43/Program.cs
@@ -0,0 +1,24 @@
+﻿// №43 Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double Read_Data(string msg) //метод для получения числа
+{
+    Console.Write(msg);
+    double num = double.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+void Intersection_Point (double k1, double b1, double k2, double b2) //метод для определния точки пересечения
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    // Вывод результатов
+    Console.WriteLine($"Точка пересечения: ({x.ToString("F2")}, {y.ToString("F2")})");
+}
+
+Console.WriteLine("Последовательно введите значения k1, b1, k2 и b2.");
+double k1 =  Read_Data ("k1 = ");
+double b1 =  Read_Data ("b1 = ");
+double k2 =  Read_Data ("k2 = ");
+double b2 =  Read_Data ("b2 = ");
+Intersection_Point (k1,b1,k2,b2);
