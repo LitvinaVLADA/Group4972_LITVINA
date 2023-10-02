a2ad967bbb46bca1354e24b0ffeb27f62dff29b9
LitvinaVlada <vladamay@outlook.com>
2023-10-02 21:14:10 +0300

    seminar8_part2

---
+++ seminar8_task59/Program.cs
@@ -0,0 +1,89 @@
+﻿// Задача 59: Задайте двумерный массив из целых чисел. 
+// Напишите программу, которая удалит строку и столбец, на 
+// пересечении которых расположен наименьший элемент 
+// массива.
+
+int [,] Fill_2D_Array (int countRow, int countColumn, int topBorder, int downBorder) //метод для рандомной генерации двумерного массива
+{
+    System.Random rnd = new System.Random();
+    int[,] array2D = new int[countRow, countColumn];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i,j] = rnd.Next(downBorder,topBorder);
+        }
+    }
+
+    return array2D;
+}
+
+void Print_2D_Array(int[,] matrix) //метод для печати двумерного массива
+{
+     for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j]+" \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+(int smallestValue,  int colIndex, int rowIndex) Find_Min_Element(int[,] matrix) //метод для поиска наименьшего элемента в массиве и его индекса
+{
+    int smallestValue = int.MaxValue;
+    int rowIndex = -1;
+    int colIndex = -1;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < smallestValue)
+            {
+                smallestValue = matrix[i, j];
+                rowIndex = i;
+                colIndex = j;
+            }
+        }
+    }
+    return (smallestValue, rowIndex, colIndex);
+}
+
+
+int[,] Remove_Row_And_Column(int[,] matrix, int rowIndex, int colIndex) //метод для удаления строки и столбца из двумерного массива
+{
+    int rowCount = matrix.GetLength(0);
+    int colCount = matrix.GetLength(1);
+
+    int[,] resultMatrix = new int[rowCount - 1, colCount - 1];
+
+    for (int i = 0, newRow = 0; i < rowCount; i++)
+    {
+        if (i == rowIndex) continue; // Если индекс строки равен индексу минимального, его пропускаем
+
+        for (int j = 0, newCol = 0; j < colCount; j++)
+        {
+            if (j == colIndex) continue; // Если индекс столбца равен индексу минимального, его пропускаем
+
+            resultMatrix[newRow, newCol] = matrix[i, j];
+            newCol++;
+        }
+        newRow++;
+    }
+
+    return resultMatrix;
+}
+
+int[,] arr_2D = Fill_2D_Array(5,5,200,10);
+Print_2D_Array(arr_2D);
+Console.WriteLine();
+(int smallestValue, int colIndex, int rowIndex) min = Find_Min_Element (arr_2D);
+Console.WriteLine ($"Наименьший элемент: {min.smallestValue} позиция [{min.rowIndex},{min.colIndex}]");
+Console.WriteLine();
+int[,] arr_2D_result = Remove_Row_And_Column (arr_2D,min.colIndex,min.rowIndex);
+Console.WriteLine("Итоговый массив:");
+Print_2D_Array(arr_2D_result);
+
---
+++ seminar8_task61/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 61: Вывести первые N строк треугольника 
+// Паскаля. Сделать вывод в виде равнобедренного 
+// треугольника
+
+int Read_Data(string start) //метод для получения количества строк
+{
+    Console.Write(start);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+//n!/k!*(n-k)!
+long Factoreal(int n)
+{
+    long res = 1;
+    for(int i=1;i<=n;i++)
+    {
+        res=res*i;
+    }
+    return res;
+}
+
+void Print_Pascal_Triangle(int nRow) //метод для вывода треугольника Паскаля на экран где каждая строчка будет печататься разным цветом
+{
+    ConsoleColor[] colors = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+    for(int i=0;i<nRow;i++)
+    {
+        Console.ForegroundColor = colors[new System.Random().Next(0,16)];
+        for(int k =0;k<nRow-i;k++)
+        {
+            Console.Write(" ");
+        }
+
+        for(int j =0; j<=i;j++)
+        {
+           Console.Write(" ");
+           
+           Console.Write(Factoreal(i)/(Factoreal(j)*Factoreal(i-j)));
+           
+        }
+        Console.ResetColor();
+        Console.WriteLine();
+    }
+}
+
+int countRow = Read_Data("Введите количество строк треугольника Паскаля: ");
+Print_Pascal_Triangle(countRow);
